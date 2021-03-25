6447a08c7b2e24e4a8bc2e402dde70e76cb7818e
Brianhin94 <brianhin64@gmail.com>
2021-03-24 20:40:38 -0400

    hello

---
+++ src/resumeData.js
@@ -0,0 +1,64 @@
+let resumeData = {
+    "name": "Brian Hinchey",
+    "role": "Frontend Developer",
+    "roleDescription": "A driven and well versed Wine expert with a passion for coding. I strive to deliver a product that exceeds a clients expectation in a timely manor that both impresses and creates room for further expansion.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/brian-hinchey/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/Brianhin94",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"A recent Graduate of General Assembly's Software Engineering immersive with a drive to deliver a well designed product to every client.",
+    "address":"Philadelphia, PA",
+    "website":"https://github.com/Brianhin94",
+    "education":[
+      {
+        "UniversityName":"General Assembly",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2021",
+      }, 
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ]
+  }
+  
+  export default resumeData
