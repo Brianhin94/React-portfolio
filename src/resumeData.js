74d41896e4fbd50852406fd81488819302178da2
Brianhin94 <brianhin64@gmail.com>
2021-03-26 13:54:12 -0400

    href

--- src/resumeData.js
+++ src/resumeData.js
@@ -37,28 +37,6 @@ let resumeData = {
         "skillname":"Reactjs"
       }
     ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ]
   }
   
   export default resumeData
